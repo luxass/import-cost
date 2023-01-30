0a4ea3e8045bec0d555f09cee72e9668e2ff1dff
Lucas Nørgård <lucasnrgaard@gmail.com>
2023-01-30 23:27:07 +0100

    chore: started on web

--- src/scan.ts
+++ src/scan.ts
@@ -6,7 +6,25 @@ import { config } from "./configuration";
 import { calculateCost } from "./engine";
 import type { Language } from "./engine";
 import { log } from "./log";
-import { isAllowedLanguage } from "./utils";
+
+function isAllowedLanguage(language: string, fileName: string): boolean {
+  return (
+    language === "javascriptreact" ||
+    fileName.endsWith(".jsx") ||
+    language === "typescriptreact" ||
+    fileName.endsWith(".tsx") ||
+    language === "javascript" ||
+    fileName.endsWith(".js") ||
+    language === "typescript" ||
+    fileName.endsWith(".ts") ||
+    language === "svelte" ||
+    fileName.endsWith(".svelte") ||
+    language === "astro" ||
+    fileName.endsWith(".astro") ||
+    language === "vue" ||
+    fileName.endsWith(".vue")
+  );
+}
 
 export async function scan(document: TextDocument | undefined) {
   if (document && config.get("enable")) {
--- tsup.config.ts
+++ tsup.config.ts
@@ -1,12 +1,24 @@
 import { defineConfig } from "tsup";
 
-export default defineConfig({
-  entry: ["src/extension.ts"],
-  format: ["cjs"],
-  clean: true,
-  treeshake: true,
-  bundle: true,
-  target: ["es2020", "chrome91", "node16"],
-  external: ["vscode"],
-  tsconfig: "tsconfig.json"
-});
+export default defineConfig([
+  {
+    entry: ["src/extension.ts"],
+    format: ["cjs"],
+    clean: true,
+    treeshake: true,
+    bundle: true,
+    target: ["es2020", "chrome91", "node16"],
+    external: ["vscode"],
+    tsconfig: "tsconfig.json"
+  },
+  {
+    entry: ["src/extension.ts"],
+    format: ["cjs"],
+    clean: true,
+    treeshake: true,
+    bundle: true,
+    target: ["es2020", "chrome91", "node16"],
+    external: ["vscode"],
+    tsconfig: "tsconfig.web.json"
+  }
+]);
