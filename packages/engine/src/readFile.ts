ada25ba49387089e0b2f09a1214b67e8c880d6ec
Lucas Nørgård <lucasnrgaard@gmail.com>
2023-02-27 20:39:03 +0100

    cleanup

--- packages/engine/src/readFile.ts
+++
@@ -1,5 +0,0 @@
-import { readFile as nodeReadFile } from "node:fs/promises";
-
-export default function readFile(path: string) {
-  return nodeReadFile(path, "utf-8");
-}
--- packages/engine/tests/shared.ts
+++
@@ -1,9 +0,0 @@
-import path from "node:path";
-
-type FixtureName = "parser" | "traverse";
-
-export function createFixture(fixtureName: FixtureName) {
-  return (fileName: string, ...args: string[]) => {
-    return path.join(__dirname, "fixtures", fixtureName, fileName, ...args);
-  };
-}
